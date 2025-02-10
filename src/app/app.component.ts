7a644dce06bc004dec2977684764b0ecdbc314ec
NikolaPetricevic <petricevicnikola64@gmail.com>
2025-02-10 13:22:33 +0100

    Commit début composant customer_reviews

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { HomeConsultComponent } from './home-consult/home-consult.component';
 import { ServicesComponent } from './services/services.component';
 import { FooterComponent } from './footer/footer.component';
+import { CustomerReviewsComponent } from './customer-reviews/customer-reviews.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeConsultComponent, ServicesComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, HomeConsultComponent, ServicesComponent, FooterComponent, CustomerReviewsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
