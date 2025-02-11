b8fadbacb53ed32b702750600bef8b44f9c77f05
NikolaPetricevic <petricevicnikola64@gmail.com>
2025-02-11 13:23:23 +0100

    Commit Fix Customer Reviews

--- src/app/customer-reviews/customer-reviews-panel/customer-reviews-panel.component.ts
+++ src/app/customer-reviews/customer-reviews-panel/customer-reviews-panel.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
-export type Customer_Review = {
-  
+export type CustomerReview = {
+  customer_name: string;
+  customer_description: string;
+  imageUrl: string;
+  review: string;
 }
 
 @Component({
@@ -12,13 +15,33 @@ export type Customer_Review = {
   styleUrl: './customer-reviews-panel.component.css'
 })
 export class CustomerReviewsPanelComponent {
-  profile_names:string[] = ["Customer 1", "Customer 2", "Customer 3"];
-  profile_descriptions:string[] = ["Desc 1", "Desc 2", "Desc 3"];
-  profile_images:string[] = ["lego_1", "lego_2", "lego_3"];
-  reviews:string[] = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et risus consequat est pretium efficitur eu sed lectus. Donec et tellus id nulla molestie congue. Duis convallis urna a lorem condimentum, vitae consequat sapien efficitur.",
-             "Cras ut arcu in tortor mattis aliquet ut vitae libero. Sed eleifend eros purus, id posuere orci faucibus non. Sed non nunc justo.",
-             "In hac habitasse platea dictumst. In hac habitasse platea dictumst. Suspendisse tempor imperdiet ante, vitae vulputate orci aliquam at."];
+  profile_names:string[] = ["",  ];
+  profile_descriptions:string[] = [];
+  profile_images:string[] = [  ];
+  raeviews:string[] = [
+             
+             ];
 
   indexProfile:number = 0;
   
+  reviews: CustomerReview[] = [
+    {
+      customer_name: "Customer 1",
+      customer_description: "Desc 2", 
+      imageUrl: "lego_1",
+      review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque et risus consequat est pretium efficitur eu sed lectus. Donec et tellus id nulla molestie congue. Duis convallis urna a lorem condimentum, vitae consequat sapien efficitur."
+    },
+    {
+      customer_name: "Customer 2",
+      customer_description: "Desc 1",
+      imageUrl: "lego_2",
+      review: "Cras ut arcu in tortor mattis aliquet ut vitae libero. Sed eleifend eros purus, id posuere orci faucibus non. Sed non nunc justo."
+    },
+    {
+      customer_name: "Customer 3",
+      customer_description: "Desc 3",
+      imageUrl: "lego_3",
+      review: "In hac habitasse platea dictumst. In hac habitasse platea dictumst. Suspendisse tempor imperdiet ante, vitae vulputate orci aliquam at."
+    }
+  ]
 }
